adb78b4df6d14b37f5ef669bb8b6bdda0e7b803e
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-14 12:28:46 +0200

    Update ft_strcpy.c

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -1,20 +1,3 @@
-#include <unistd.h>
-#include <stdlib.h>
-
-void ft_putchar(char c)
-{
-    write(1,&c,1);
-}
-void ft_putstr(char *str)
-{
-    int count;
-    count = 0;
-    while(str[count])
-    {
-        ft_putchar(str[count]);
-        count++;
-    }
-}
 char *ft_strcpy(char *dest, char *src)
 {
     int count;
